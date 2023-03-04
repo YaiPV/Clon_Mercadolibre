c8a53af5e9616630dd7ae7b1c1b1c665c9a9c3a0
YaiPV <ypv308@gmail.com>
2023-03-04 12:04:25 -0500

    git commit -m "Changed the java script format for the sales section."

--- js/categories-container.js
+++ js/categories-container.js
@@ -1,69 +1,136 @@
+function getCategoriesData() {
+  const data = [
+    {
+      image: "./img/car.png",
+      description: `<p class="categories-description">Carros, Motos y Otros</p>`,
+    },
+    {
+      image: "./img/compu.png",
+      description: `<p class="categories-description">Computación</p>`,
+    },
+    {
+      image: "./img/tshirt.png",
+      description: `<p class="categories-description">Ropa y Accesorios</p>`,
+    },
+    {
+      image: "./img/sofa.png",
+      description: `<p class="categories-descriptionS">Hogar y Muebles</p>`,
+    },
+    {
+      image: "./img/carwheel.png",
+      description: ` <p class="categories-description">Accesorios para Vehículos</p>`,
+    },
+    {
+      image: "./img/stove.png",
+      description: `<p class="categories-description">Electrodomésticos</p>`,
+    },
+    {
+      image: "./img/camara.png",
+      description: `<p class="categories-description">Cámaras y Accesorios</p>`,
+    },
+    {
+      image: "./img/cellphone.png",
+      description: `<p class="categories-description">Celulares y Teléfonos</p>`,
+    },
+    {
+      image: "./img/lipstick.png",
+      description: `<p class="categories-description">Belleza y Cuidado Personal</p>`,
+    },
+    {
+      image: "./img/football.png",
+      description: `<p class="categories-description">Deportes y Fitness</p>`,
+    },
+    {
+      image: "./img/microphone.png",
+      description: `<p class="categories-description">Electrónica, Audios y Video</p>`,
+    },
+    {
+      image: "./img/videogame.png",
+      description: `<p class="categories-description">Consolas y Videojuegos</p>`,
+    },
+    {
+      image: "./img/bear.png",
+      description: `<p class="categories-description">Juegos y Juguetes</p>`,
+    },
+    {
+      image: "./img/tool.png",
+      description: `<p class="categories-description">Herramientas</p>`,
+    },
+  ];
+  return data;
+}
+
 function categoriesContainer() {
+  const categoriesData = getCategoriesData();
   return `      <div>
   <h1 class="categories-header">Categorias populares</h1>
+  ${
+    categoriesData.map((item))=>`
+    `
+  }
 </div>
 <div>
   <div class="flex space-around card-image-icon">
     <div class="categories-picture">
-      <img src="./img/car.png" />
-      <p class="categories-description">Carros, Motos y Otros</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/compu.png" />
-      <p class="categories-description">Computación</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/tshirt.png" />
-      <p class="categories-description">Ropa y Accesorios</p>
+      <img src="" />
+      
     </div>
 
     <div class="categories-picture">
-      <img src="./img/sofa.png" />
-      <p class="categories-descriptionS">Hogar y Muebles</p>
+      <img src=""/>
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/carwheel.png" />
-      <p class="categories-description">Accesorios para Vehículos</p>
+      <img src="" />
+     
     </div>
     <div class="categories-picture">
-      <img src="./img/stove.png" />
-      <p class="categories-description">Electrodomésticos</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/camara.png" />
-      <p class="categories-description">Cámaras y Accesorios</p>
+      <img src="" />
+      
     </div>
   </div>
 
   <div class="flex space-around card-image-icon">
     <div class="categories-picture">
-      <img src="./img/cellphone.png" />
-      <p class="categories-description">Celulares y Teléfonos</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/lipstick.png" />
-      <p class="categories-description">Belleza y Cuidado Personal</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/football.png" />
-      <p class="categories-description">Deportes y Fitness</p>
+      <img src="" />
+      
     </div>
 
     <div class="categories-picture">
-      <img src="./img/microphone.png" />
-      <p class="categories-description">Electrónica, Audios y Video</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/videogame.png" />
-      <p class="categories-description">Consolas y Videojuegos</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/bear.png" />
-      <p class="categories-description">Juegos y Juguetes</p>
+      <img src="" />
+      
     </div>
     <div class="categories-picture">
-      <img src="./img/tool.png" />
-      <p class="categories-description">Herramientas</p>
+      <img src="" />
+      
     </div>
   </div>
 </div>
